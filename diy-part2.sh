5756077c85b7853aae029c3f5a7f0cdc15f0e44c
zhangdi <616620131@qq.com>
2020-12-22 23:45:09 +0800

    fix

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,11 +12,11 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
-sed -i '/set network.globals.ula_prefix/d' openwrt/package/base-files/files/bin/config_generate
-echo "DISTRIB_REVISION='Compiled by zhangdi '" > openwrt/package/base-files/files/etc/openwrt_release1
-curl -fsSL  https://raw.githubusercontent.com/zzd/other/master/patch/poweroff/poweroff.htm > openwrt/feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_system/poweroff.htm 
-curl -fsSL  https://raw.githubusercontent.com/zzd/other/master/patch/poweroff/system.lua > openwrt/feeds/luci/modules/luci-mod-admin-full/luasrc/controller/admin/system.lua
+sed -i '/set network.globals.ula_prefix/d' package/base-files/files/bin/config_generate
+echo "DISTRIB_REVISION='Compiled by zhangdi '" > package/base-files/files/etc/openwrt_release1
+curl -fsSL  https://raw.githubusercontent.com/zzd/other/master/patch/poweroff/poweroff.htm > feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_system/poweroff.htm 
+curl -fsSL  https://raw.githubusercontent.com/zzd/other/master/patch/poweroff/system.lua > feeds/luci/modules/luci-mod-admin-full/luasrc/controller/admin/system.lua
 # sed -i 's/网络存储/存储/g' package/lean/luci-app-vsftpd/po/zh-cn/vsftpd.po
 sed -i 's/Turbo ACC 网络加速/ACC网络加速/g' package/lean/luci-app-sfe/po/zh-cn/sfe.po
 # sed -i 's/带宽监控/监控/g' feeds/luci/applications/luci-app-nlbwmon/po/zh-cn/nlbwmon.po
-./openwrt/scripts/feeds update -i
+./scripts/feeds update -i
